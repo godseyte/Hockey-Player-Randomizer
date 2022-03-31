e800b439dfaa703fe5937abb416bfbee6968839c
Tucker Godsey <tucker.godsey@gmail.com>
2022-03-31 17:04:50 -0400

    begining functionality

--- src/playerGenerator.js
+++ src/playerGenerator.js
@@ -16,23 +16,51 @@ let teamName = "";
 let position = "";
 let handed = "";
 
+// Get Documents to print stats to
+const docFirstName = document.getElementById("firstName");
+const docLastName = document.getElementById("lastName");
+const docJerseyNumber = document.getElementById("jerseyNumber");
+const docHeight = document.getElementById("height");
+const docWeight = document.getElementById("weight");
+const docSex = document.getElementById("sex");
+const docCity = document.getElementById("city");
+const docTeamName = document.getElementById("teamName");
+const docPosition = document.getElementById("position");
+const docHanded = document.getElementById("handed");
+
+// TEMP: Generate a Player
+GeneratePlayer();
+
 // Generate a Hockey Player
 function GeneratePlayer() 
 {
     firstName = "Patrik";
     lastName = "Laine";
     jerseyNumber = "29";
-    height = 77;
-    weight = 216;
+    height = 77 + "inch";
+    weight = 216 + "lbs";
     sex = "yes";
     city = "Columbus";
     teamName = "Blue Jackets";
     position = "Left Wing";
     handed = "Right";
+
+    // TEMP: Return Player
+    ReturnPlayer(firstName, lastName, jerseyNumber, height, weight, sex, city, teamName, position, handed);
 }
 
 // Return a Player
 function ReturnPlayer(firstName, lastName, jerseyNumber, height, weight, sex, city, teamName, position, handed)
 {
+    docFirstName.innerHTML = firstName;
+    docLastName.innerHTML = lastName;
+    docJerseyNumber.innerHTML = jerseyNumber;
+    docHeight.innerHTML = height;
+    docWeight.innerHTML = weight;
+    docSex.innerHTML = sex;
+    docCity.innerHTML = city;
+    docTeamName.innerHTML = teamName;
+    docPosition.innerHTML = position;
+    docHanded.innerHTML = handed;
     
 }
