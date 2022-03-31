dd170cd2bf7e8d610d75d43aa1f0988e8c00863c
Tucker Godsey <tucker.godsey@gmail.com>
2022-03-31 16:50:18 -0400

    init commit

---
+++ src/playerGenerator.js
@@ -0,0 +1,38 @@
+/*
+Written by Tucker Godsey
+
+Generates a Hockey Player using randomized variables.
+*/
+
+// Variables
+let firstName = "";
+let lastName = "";
+let jerseyNumber = 0;
+let height = 0; // In inches
+let weight = 0; // In pounds
+let sex = "";
+let city = "";
+let teamName = "";
+let position = "";
+let handed = "";
+
+// Generate a Hockey Player
+function GeneratePlayer() 
+{
+    firstName = "Patrik";
+    lastName = "Laine";
+    jerseyNumber = "29";
+    height = 77;
+    weight = 216;
+    sex = "yes";
+    city = "Columbus";
+    teamName = "Blue Jackets";
+    position = "Left Wing";
+    handed = "Right";
+}
+
+// Return a Player
+function ReturnPlayer(firstName, lastName, jerseyNumber, height, weight, sex, city, teamName, position, handed)
+{
+    
+}
