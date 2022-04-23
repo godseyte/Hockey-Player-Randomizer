c5b415894a79b684830a226e8e57bc681b07a5dc
Tucker Godsey <tucker.godsey@gmail.com>
2022-04-23 14:17:38 -0400

    A few more names

--- src/nameGenerator.js
+++ src/nameGenerator.js
@@ -21,10 +21,10 @@ function GenerateNameArrays()
 {
     maleNames = ['Partrik', 'Jakub', 'Boone', 'Oliver', 'Zack', 'Emil', 'Yegor', 'Justin', 'Brendan', 'Sean', 'Carson', 'Gustav', 'Eric', 'Jack', 'Cole', 'Alexandre', 
     'Gavin', 'Jake', 'Adam', 'Gabriel', 'Vladislav', 'Dean', 'Andrew', 'J-F', 'Joonas', 'Elivs', 'Daniil', 'Brad', 'Cam', 'Kris', 'Kent', 'Bobert', 'Robert', 'John', 'Gerald',
-    'Aidan', 'Aiden'];
+    'Aidan', 'Aiden', 'Pierre', 'Alexander', 'Jason', 'Jeremy', 'Jeremiah', 'Bryce', 'Tucker'];
 
     femaleNames = ['Cynthia', 'Scarlet', 'Olivia', 'Emma', 'Ava', 'Charlotte', 'Sarah', 'Ana', 'Cindy', 'Carol', 'Hannah', 'Natasha', 'Nyatasha', 'Azuki', 'Yui', 'Asuna',
-    'Kaguya', 'Lexy', 'Jaden', 'Mary'];
+    'Kaguya', 'Lexy', 'Jaden', 'Mary', 'Sylvia', 'Samantha', 'Haley', 'Harley', 'Ivy', 'Mel'];
     
     otherNames = ['Alex', 'Nico', 'Lex', 'Baily', 'Dylan', 'Joe', 'Arlo', 'Zane', 'Atlas', 'Whitley', 'Kane', 'Adian', 'Ashton', 'Blaine', 'Blair', 'Cameron', 'Colby',
     'Dakota', 'Dawson', 'Emery', 'Hunter', 'Julian', 'Peyton', 'Quinn', 'Sidney', 'Dalton', 'Mevlin']
