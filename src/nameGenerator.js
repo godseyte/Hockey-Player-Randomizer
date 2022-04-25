f51f8758766f11bc7da7d28c6ad366a99e915840
Tucker Godsey <tucker.godsey@gmail.com>
2022-04-24 21:55:33 -0400

    We've got more names baby

--- src/nameGenerator.js
+++ src/nameGenerator.js
@@ -10,9 +10,11 @@ let femaleNames = [];
 let otherNames = [];
 let allNames = [];
 
+GenerateNameArrays();
+
 function GenerateName(sex)
 {
-    GenerateNameArrays();
+    
     console.log(maleNames + femaleNames + otherNames)
     console.log(allNames)
 }
@@ -21,10 +23,10 @@ function GenerateNameArrays()
 {
     maleNames = ['Partrik', 'Jakub', 'Boone', 'Oliver', 'Zack', 'Emil', 'Yegor', 'Justin', 'Brendan', 'Sean', 'Carson', 'Gustav', 'Eric', 'Jack', 'Cole', 'Alexandre', 
     'Gavin', 'Jake', 'Adam', 'Gabriel', 'Vladislav', 'Dean', 'Andrew', 'J-F', 'Joonas', 'Elivs', 'Daniil', 'Brad', 'Cam', 'Kris', 'Kent', 'Bobert', 'Robert', 'John', 'Gerald',
-    'Aidan', 'Aiden', 'Pierre', 'Alexander', 'Jason', 'Jeremy', 'Jeremiah', 'Bryce', 'Tucker'];
+    'Aidan', 'Aiden', 'Pierre', 'Alexander', 'Jason', 'Jeremy', 'Jeremiah', 'Bryce', 'Tucker', 'Gary', 'Will', 'William'];
 
     femaleNames = ['Cynthia', 'Scarlet', 'Olivia', 'Emma', 'Ava', 'Charlotte', 'Sarah', 'Ana', 'Cindy', 'Carol', 'Hannah', 'Natasha', 'Nyatasha', 'Azuki', 'Yui', 'Asuna',
-    'Kaguya', 'Lexy', 'Jaden', 'Mary', 'Sylvia', 'Samantha', 'Haley', 'Harley', 'Ivy', 'Mel'];
+    'Kaguya', 'Lexy', 'Jaden', 'Mary', 'Sylvia', 'Samantha', 'Haley', 'Harley', 'Ivy', 'Mel', 'Angela', 'Yuna', 'Miko', 'Aloy'];
     
     otherNames = ['Alex', 'Nico', 'Lex', 'Baily', 'Dylan', 'Joe', 'Arlo', 'Zane', 'Atlas', 'Whitley', 'Kane', 'Adian', 'Ashton', 'Blaine', 'Blair', 'Cameron', 'Colby',
     'Dakota', 'Dawson', 'Emery', 'Hunter', 'Julian', 'Peyton', 'Quinn', 'Sidney', 'Dalton', 'Mevlin']
